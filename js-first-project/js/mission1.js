301804ec3bf054057b242462e3d241f1cd128a6d
tarzanshen <31402235+tarzanshen@users.noreply.github.com>
2019-07-04 20:44:33 +0800

    Add files via upload

--- js-first-project/js/mission1.js
+++ js-first-project/js/mission1.js
@@ -1,6 +1,4 @@
-function number(n, m) {
-  return Math.round(Math.random() * (m - n));
-}
+
 
 /*function color16() {
   var r = Math.floor(Math.random() * 256);
@@ -11,13 +9,14 @@ function number(n, m) {
   return rgb;
 
 }*/
-function color16(){
+function color16(){//随机16位颜色代码
   var color = "#";
   for( var i = 0; i < 6; i++ ){
       color += Math.floor( Math.random()*16).toString(16);   
   }
-  //console.log(color);
   return color; 
+  //console.log(color);
+  
 }
 /*
 var tenNumber =new Array;
@@ -31,60 +30,53 @@ for(var i=0;i<3;i++){
   }
    
 }*/
-function colors() {
-  for(var a=0;a<9;a++) {
+function colors() {//点击函数
+  for(var a=0;a<9;a++) {//颜色归位
     document.getElementsByClassName("div1")[a].style.backgroundColor="#FFC107";
-    }
-  var tenNumber = new Array;
+  }
+  var tenNumber = new Array;//三个随机数字
   for (var i = 0; i < 3; i++) {
-    var x = number(1, 9);
+    var x = Math.round(Math.random() * 8);
     if (-1 == tenNumber.indexOf(x)) {
       tenNumber.push(x);
-    } else {
+    } 
+    else {
       i--;
     }
-
   }
+
   var x;
-  var y;
-  for (x in tenNumber) {
+  for (x in tenNumber) {//dom绑定随机数字
     document.getElementsByClassName("div1")[tenNumber[x]].style.backgroundColor = color16();
   }
 }
 
-
-function bgColor() {
-  for(var a=0;a<9;a++) {
-   document.getElementsByClassName("div1")[a].style.backgroundColor="#FFC107";
-   }
-}
-
 var timer=null;
 var c = 0
-function click1() {
+function click1() {//触发器
   c++;
-  console.log(c);
   if(c==1){
   start();
   }
 }
-function start(){
-  
-  document.getElementById("button").style.backgroundColor="#FFC107";
-  document.getElementById("button").style.color="#ffffff";
-  document.getElementById("button2").style.backgroundColor="#ffffff";
-  document.getElementById("button2").style.color="#FFC107";
-timer=setInterval(colors,1000);
-
+function start() {
+  var buttonColor= document.getElementById("button")
+  var buttonColor2= document.getElementById("button2")
+  buttonColor.style.backgroundColor="#FFC107";//上部按钮文字颜色颜色
+  buttonColor.style.color="#ffffff";
+  buttonColor2.style.backgroundColor="#ffffff";
+  buttonColor2.style.color="#FFC107";
+timer=setInterval("colors()",1000);
 //timer2=setInterval(bgColor,1850);
-
 }
 
 function end() {
-  document.getElementById("button2").style.backgroundColor="#FFC107";
-  document.getElementById("button2").style.color="#ffffff";
-  document.getElementById("button").style.backgroundColor="#ffffff";
-  document.getElementById("button").style.color="#FFC107";
+  var buttonColor= document.getElementById("button")
+  var buttonColor2= document.getElementById("button2")
+  buttonColor2.style.backgroundColor="#FFC107";//下部按钮文字
+  buttonColor2.style.color="#ffffff";
+  buttonColor.style.backgroundColor="#ffffff";
+  buttonColor.style.color="#FFC107";
   clearInterval(timer);
   for(var a=0;a<9;a++) {
     document.getElementsByClassName("div1")[a].style.backgroundColor="#FFC107";
